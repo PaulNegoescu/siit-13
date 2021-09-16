365553eb0bffe33d8c52cde0c0b4aab5b52c0a13
Paul Negoescu <paul.negoescu@gmail.com>
2021-09-16 21:09:06 +0300

    Basic react app

---
+++ src/Counter.js
@@ -0,0 +1,14 @@
+function Counter() {
+  return (
+    <>
+      <h1>Counter</h1>
+      <p>
+        <output>0</output>
+      </p>
+      <button>-</button>
+      <button>+</button>
+    </>
+  );
+}
+
+export default Counter;
