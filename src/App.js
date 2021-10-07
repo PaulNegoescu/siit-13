d28a74af72e1e09ff1109fe009f1177c1c3a800d
Paul Negoescu <paul.negoescu@gmail.com>
2021-10-07 19:36:16 +0300

    React router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-// import { Parent } from './Communication/Parent';
-// import { Counter } from './Counter/Counter';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Parent } from './features/Communication/Parent';
+import { Counter } from './features/Counter/Counter';
 import { TodoList } from './features/Todos/TodoList';
+import { Nav } from './components/Nav/Nav';
 import './App.css';
 
 function App() {
-  return <TodoList />;
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Counter} />
+        <Route path="/communication" component={Parent} />
+        <Route path="/todos" component={TodoList} />
+        <Route path="*" component={() => <h1>404 Page not found</h1>} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,29 @@
+import { Link, NavLink } from 'react-router-dom';
+import styles from './Nav.module.css';
+
+export function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <Link to="/" className={styles.logo}>
+        My Awesome Site
+      </Link>
+      <ul className={styles['main-menu']}>
+        <li>
+          <NavLink exact to="/" activeClassName={styles.active}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/communication" activeClassName={styles.active}>
+            Communication
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/todos" activeClassName={styles.active}>
+            Todos
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
